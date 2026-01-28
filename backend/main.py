ea7c84fa6e61408bfd375eeced123960807d4282
David Yang <yang200023@gmail.com>
2026-01-28 22:13:09 +1100

    adding auth related router

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from utils.database import engine, Base
 
+from api.auth import router as auth_router
+
 app = FastAPI()
+app.include_router(auth_router)
 
 # Create all database tables
 Base.metadata.create_all(bind=engine)
