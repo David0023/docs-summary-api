de4d5027f9c5323166fb78e86635f90ed52f8500
David Yang <yang200023@gmail.com>
2026-01-28 22:13:52 +1100

    adding user db model

---
+++ backend/models/base.py
@@ -0,0 +1,10 @@
+from datetime import datetime, timezone
+from sqlalchemy import Column, DateTime
+from utils.database import Base
+
+class TimestampMixin:
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+
+class BaseModel(Base):
+    __abstract__ = True
